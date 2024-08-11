40fa63a6dfc15a9e782b1c224cd28de64b469716
DON-KISON_PC\isaev <isaevasvoenmeh@gmail.com>
2024-08-12 00:40:46 +0300

    Some refactoring

---
+++ Niias.Application/DomainExtentions/RailwayParkExtention.cs
@@ -0,0 +1,31 @@
+﻿using Niias.Domain;
+
+namespace Niias.Application.DomainExtentions;
+
+public static class RailwayParkExtention
+{
+    public static List<RailwayPoint> GetAllPoints(this RailwayPark park)
+    {
+        //Тут используем словарь потому что одна и таже точка может быть частью
+        //нескольких путей, поэтому обезопасим себя добавляя только уникальные точки
+        Dictionary<Guid, RailwayPoint> parkPoints = new();
+
+        //Собираем все точки в парке.
+        //Параллельно составляем список самых восточных из них
+        foreach (var route in park.Routes)
+        {
+            foreach (var section in route.RailwaySections)
+            {
+                foreach (var point in section.Points)
+                {
+                    if (!parkPoints.ContainsKey(point.Id))
+                    {
+                        parkPoints.Add(point.Id, point);
+                    }
+                }
+            }
+        }
+
+        return parkPoints.Values.ToList();
+    }
+}
---
+++ Task2_ShortestWayFinding/Program.cs
@@ -0,0 +1,67 @@
+﻿using Niias.Application;
+using Niias.Domain;
+using Niias.Infrastructure.HardcodedParkProvider;
+
+namespace Task2_ShortestWayFinding;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        RailwayScheme railwayScheme = (new HardcodedRailwaySchemeProvider()).GetRailwayPark();
+
+        Console.WriteLine($"There are {railwayScheme.RailwaySections.Count} sections in scheme:");
+
+        for(int i = 0; i < railwayScheme.RailwaySections.Count; i++)
+        {
+            Console.WriteLine($"Section #[{i + 1}] Guid = {railwayScheme.RailwaySections[i].Guid}");
+        }
+
+        Console.WriteLine();
+        int from = -1; 
+        int to = -1;
+        Console.WriteLine("Let's find shortest way!");
+        Console.WriteLine("Enter the start section number:");
+        string fromStr = Console.ReadLine();
+        if(!Int32.TryParse(fromStr, out from) || from < 1 || from > railwayScheme.RailwaySections.Count)
+        {
+            Console.WriteLine("Wrong number! Press any key to exit...");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("Enter the destination section number:");
+        string toStr = Console.ReadLine();
+        if (!Int32.TryParse(toStr, out to) || to < 1 || to > railwayScheme.RailwaySections.Count)
+        {
+            Console.WriteLine("Wrong number! Press any key to exit...");
+            Console.ReadKey();
+            return;
+        }
+
+        var shortestWay = PathFinderService.GetShortestPath(railwayScheme, railwayScheme.RailwaySections[from], railwayScheme.RailwaySections[to]);
+
+        if (shortestWay.Count == 0)
+        {
+            Console.WriteLine("There is no way.");
+        }
+        else
+        {
+            Console.WriteLine($"The shortest way contains {shortestWay.Count} sections:");
+            for (int i = 0; i < shortestWay.Count; i++)
+            {
+                Console.WriteLine($"Section #{i}: Id - {shortestWay[i].Guid}");
+            }
+        }
+
+
+        WriteSeparator();
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    private static void WriteSeparator()
+    {
+        Console.WriteLine(new String('-', 100));
+    }
+}
